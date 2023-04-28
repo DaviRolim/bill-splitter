793047d9f38fda2bfbf96105fa9bdede318f690a
Davi Rolim <davirolim94@gmail.com>
2023-04-28 17:46:06 -0300

    added starting code for splitted bill component

--- packages/bill-splitter/app/page.tsx
+++ packages/bill-splitter/app/page.tsx
@@ -3,16 +3,53 @@ import CameraComponent from '@/components/Camera';
 import AddFriend from '@/components/AddFriend';
 import { useState } from 'react';
 import FriendList from '@/components/FriendList';
+import { AnalyzedItem } from '@/types/analyzedItem';
+import ItemList from '@/components/SplittedBill';
 
+const items: AnalyzedItem[] = [
+  {
+    quantity: 1,
+    name: 'Spaghetti Carbonara',
+    unitPrice: 12.99,
+    price: 12.99,
+  },
+  {
+    quantity: 2,
+    name: 'Caesar Salad',
+    unitPrice: 8.99,
+    price: 17.98,
+  },
+  {
+    quantity: 1,
+    name: 'New York Steak',
+    unitPrice: 24.99,
+    price: 24.99,
+  },
+  {
+    quantity: 1,
+    name: 'Chicken Alfredo',
+    unitPrice: 15.99,
+    price: 15.99,
+  },
+  {
+    quantity: 3,
+    name: 'Garlic Bread',
+    unitPrice: 3.99,
+    price: 11.97,
+  },
+];
+  
 type Friend = {
-  name: string
-}
+  name: string;
+  totalExpense: number;
+};
+
 
 export default function Home() {
   const [friends, setFriends] = useState<Friend[]>([]);
 
   const handleAddFriend = (name: string) => {
-    setFriends([...friends, { name }]);
+    setFriends([...friends, { name, totalExpense: 0 }]);
   };
 
   const handleDeleteFriend = (name: string) => {
@@ -29,6 +66,9 @@ export default function Home() {
       </div>
       <div className='mx-8'>
         <CameraComponent />
+      </div>
+      <div>
+      <ItemList items={items} friends={friends} />
       </div>
     </main>
   )
--- packages/bill-splitter/components/SplittedBill.tsx
+++ packages/bill-splitter/components/SplittedBill.tsx
@@ -1,43 +1,84 @@
 import React from 'react'
+import { useState } from "react";
 import { AnalyzedItem } from '@/types/analyzedItem';
-const items: AnalyzedItem[] = [
-    {
-      quantity: 1,
-      name: 'Spaghetti Carbonara',
-      unitPrice: 12.99,
-      price: 12.99,
-    },
-    {
-      quantity: 2,
-      name: 'Caesar Salad',
-      unitPrice: 8.99,
-      price: 17.98,
-    },
-    {
-      quantity: 1,
-      name: 'New York Steak',
-      unitPrice: 24.99,
-      price: 24.99,
-    },
-    {
-      quantity: 1,
-      name: 'Chicken Alfredo',
-      unitPrice: 15.99,
-      price: 15.99,
-    },
-    {
-      quantity: 3,
-      name: 'Garlic Bread',
-      unitPrice: 3.99,
-      price: 11.97,
-    },
-  ];
+
   
-
-export default function SplittedBill() {
-    
-  return (
-
-    <div>SplittedBill</div>
-  )
-}
+  
+  type Props = {
+    items: AnalyzedItem[];
+    friends: Friend[];
+  };
+  
+  type Friend = {
+    name: string;
+    totalExpense: number;
+  };
+  
+  const ItemList: React.FC<Props> = ({ items, friends }) => {
+    const [selectedFriends, setSelectedFriends] = useState<Friend[]>([]);
+  
+    const handleFriendSelect = (friend: Friend, index: number) => {
+      // copy the selectedFriends array to avoid mutating state directly
+      const updatedSelectedFriends = [...selectedFriends];
+      // toggle the friend's selection status
+      updatedSelectedFriends[index] = {
+        ...friend,
+        isSelected: !friend.isSelected,
+      };
+      setSelectedFriends(updatedSelectedFriends);
+    };
+  
+    const calculateFriendExpenses = (friend: Friend) => {
+      // calculate the total expense for the selected items
+      // return items.reduce((total, item) => {
+      //   const itemTotal = item.price / items.length;
+      //   if (item.friends.includes(friend.name)) {
+      //     return total + itemTotal;
+      //   }
+      //   return total;
+      // }, 0);
+    };
+  
+    return (
+      <div>
+        {items.map((item, index) => (
+          <div key={index} className="mb-4">
+            <h2 className="font-bold text-lg mb-2">{item.name}</h2>
+            <div className="flex justify-between">
+              <div>
+                <p>Quantity: {item.quantity}</p>
+                <p>Unit Price: {item.unitPrice}</p>
+                <p>Total Price: {item.price}</p>
+              </div>
+              <div>
+                {friends.map((friend, friendIndex) => (
+                  <div key={friendIndex} className="flex items-center">
+                    <input
+                      type="checkbox"
+                      // checked={
+                      //   // selectedFriends[friendIndex]?.isSelected ?? false
+                      // }
+                      onChange={() => handleFriendSelect(friend, friendIndex)}
+                      className="mr-2"
+                    />
+                    <label>{friend.name}</label>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className="mt-4">
+          <h2 className="font-bold text-lg mb-2">Friend Expenses</h2>
+          {friends.map((friend, index) => (
+            <div key={index}>
+              <p>{friend.name}: {/*{calculateFriendExpenses(friend)}*/}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default ItemList;
+  
